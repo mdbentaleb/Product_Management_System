08bea845608bf118f867077a6018b0abb5190bcc
mdbentaleb <bentalebm@outlook.com>
2025-03-22 02:28:55 +0000

    --

--- main.js
+++ main.js
@@ -17,3 +17,57 @@ btnAddProduct.addEventListener('click', () => {
         icon.src = 'imgs/down.png';
     }
 });
+
+
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let tax = document.getElementById("tax");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let submit = document.getElementById("submit");
+let totalvar;
+
+// get totale
+function getTotal() {
+	if (price.value != '')
+	{
+		totalvar = (parseInt(price.value) + +tax.value + +ads.value) - +discount.value;
+	}
+}
+
+// creat product
+let dataProduct;
+if (localStorage.product != null) {
+	dataProduct = JSON.parse(localStorage.product);
+}
+else
+	dataProduct = [];
+
+submit.onclick = function() {
+	let newProduct = {
+		title: title.value,
+		price: price.value,
+		tax: tax.value,
+		ads: ads.value,
+		discount: discount.value,
+		total: totalvar,
+		count: count.value,
+		category: category.value,
+	}
+	dataProduct.push(newProduct);
+
+	localStorage.setItem('product', JSON.stringify(dataProduct));
+	console.log(newProduct);
+}
+
+
+// save data input in local storage
+// clear data input
+// show data
+// creat cout product in same time
+// delete product or all product
+// update product
+// search product
+// clean data
